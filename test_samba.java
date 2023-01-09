2b6e4b932dbc343eee551bbf7c84855e95d96381
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-01-09 14:45:58 +0100

    contact ok

--- test_samba.java
+++ test_samba.java
@@ -1,19 +1,37 @@
 // test Samba
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class test_samba {
+    private static Scanner _scan = new Scanner (System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println (menu);
+        while (true){
+        menu();
+        String choix = _scan.nextLine();
+        switch (choix){
+            case "1":
+            break;
+            case "2":
+            break;
+            case "q":
+            return;
+            default:
+            System.out.println("réessayer avec une bonne touche");
+            break;
+        }
     }
-    public class menu{
-       public String ajouter_contact;
-       public String lister_contact;
-       public String quitter_appli; 
+    }
+    
+    public static void menu(){
+        ArrayList<String> menu =new ArrayList<>();
+        System.out.println("ajouter contact");
+        System.out.println("lister contact");
+        System.out.println("quitter l'application");
     }
 }
 
 
-
-
-//a pour ajouter un contact
-//z pour lister les contact
+//System.out.println (menu);
+//ajouter contact
+//lister contact
 //q pour quitter l'appli 
