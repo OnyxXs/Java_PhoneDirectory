d22eeb159b279c2e45b9098e6ddcf48745fc8261
OnyxXs <timeo.donnaes@gmail.com>
2023-01-09 12:25:08 +0100

    menu

--- test_Timeo.java
+++ test_Timeo.java
@@ -1,5 +1,12 @@
 public class test_Timeo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("AKALI");
+        menu Monmenu = new.menu()
+        System.out.println (menu);
+    }
+    public class menu{
+       public String ajouter_contact;
+       public String lister_contact;
+       public String quitter_appli;
     }
 }
