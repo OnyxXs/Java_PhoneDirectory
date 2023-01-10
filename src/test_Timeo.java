06cd910eef383e522b31dad21165f1ca7e1d05ac
OnyxXs <timeo.donnaes@gmail.com>
2023-01-10 10:04:39 +0100

    com

--- src/test_Timeo.java
+++ src/test_Timeo.java
@@ -1,12 +1,37 @@
-public class test_Timeo {
+// test Samba
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test_samba {
+    private static Scanner _scan = new Scanner (System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("AKALI");
-        menu Monmenu = new.menu()
-        System.out.println (menu);
+        while (true){
+        menu();
+        String choix = _scan.nextLine();
+        switch (choix){
+            case "1":
+            break;
+            case "2":
+            break;
+            case "q":
+            return;
+            default:
+            System.out.println("réessayer avec une bonne touche");
+            break;
+        }
     }
-    public class menu{
-       public String ajouter_contact;
-       public String lister_contact;
-       public String quitter_appli;
+    } 
+    
+    public static void menu(){
+        ArrayList<String> menu =new ArrayList<>();
+        System.out.println("ajouter contact");
+        System.out.println("lister contact");
+        System.out.println("quitter l'application");
     }
 }
+
+
+//System.out.println (menu);
+//ajouter contact
+//lister contact
+//q pour quitter l'appli 
